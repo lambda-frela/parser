4e8afecff9ad16c3cb98295404be1d2cea2486a9
Konstantin Anischenko <anishenko00@gmail.com>
2016-11-09 23:13:08 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -10,4 +10,8 @@ request.encoding = 'utf-8'
 soup = BeautifulSoup(request.text, "html.parser")
 top_titles = soup.find_all("a", class_="tag-book-title")
 
-with open('output.txt','w',encoding='utf8') as f:
+with open('output.txt', 'w', encoding='utf8') as f:
+    for book in list(top_titles):
+        book = str(book)
+        f.write(book[(book[:-4].index('>')+1):-4]+"\n")
+    f.close()
