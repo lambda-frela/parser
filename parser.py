13501997dfb1fa0c758bbfba850c87c8c8a5317b
Timur <murychkun@yahoo.com>
2016-11-09 16:16:20 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 
 target_url = "http://www.livelib.ru/books/top"
 
-r = requests.get(target_url)
-r.encoding = 'utf-8'
+request = requests.get(target_url)
+request.encoding = 'utf-8'
 
-soup = BeautifulSoup(r.text,  "html.parser")
+soup = BeautifulSoup(request.text, "html.parser")
 top_titles = soup.find_all("a", class_="tag-book-title")
 
 with open('output.txt','w',encoding='utf8') as f:
