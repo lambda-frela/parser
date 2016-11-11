9b7a9af2bb781ddd484655c88efad9097f8c2178
Konstantin Anischenko <anishenko00@gmail.com>
2016-11-11 17:53:59 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,7 +11,6 @@ soup = BeautifulSoup(request.text, "html.parser")
 top_titles = soup.find_all("a", class_="tag-book-title")
 
 with open('output.txt', 'w', encoding='utf8') as f:
-    for book in list(top_titles):
-        book = str(book)
-        f.write(book[(book[:-4].index('>')+1):-4]+"\n")
-    f.close()
+    for link in top_titles:
+        f.write(link.get('title'))
+        f.write('\n')
